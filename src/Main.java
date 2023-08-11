25855296e60e0330f4f0dd06791f1648a2ad6679
EmmanuelMartinez11 <emma.nionn@gmail.com>
2023-08-11 12:03:23 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1, num2, num3;
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        num1 = escaner.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        num2 = escaner.nextInt();
+        System.out.print("Ingrese el tercer número: ");
+        num3 = escaner.nextInt();
+
+        if ((num1 < num2) && (num1 < num3)) {
+            if (num2 < num3) System.out.print(num1 + " " + num2 + " " + num3);
+            else System.out.print(num1 + " " + num3 + " " + num2);
+        } else if ((num2 < num1) && (num2 < num3)) {
+            if (num1 < num3) System.out.print(num2 + " " + num1 + " " + num3);
+            else System.out.print(num2 + " " + num3 + " " + num1);
+        } else if ((num3 < num1) && (num3 < num2)) {
+            if (num1 < num2) System.out.print(num3 + " " + num1 + " " + num2);
+            else System.out.print(num3 + " " + num2 + " " + num1);
+        } else System.out.print(num1 + " " + num2 + " " + num3);
     }
 }
